03508c1da3165989833170eca9a890a4892e2c27
BRAYDENS-GAMING\brayd <brayden.schultz.5@gmail.com>
2018-07-25 20:09:07 +0930

    fixed build.sh again

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-sudo apt -y install gcc libboost-all-dev build-essential cmake pkg-config libssl-dev gpp
 sudo apt update
+sudo apt -y install gcc libboost-all-dev build-essential cmake pkg-config libssl-dev gpp
 sudo make

4d34ad1d84e7a9356a3d6fba764dec39173a30db
lordtenteth <brayden.schultz.5@gmail.com>
2018-08-02 21:10:53 +0930

    Update desktopbuild.sh

--- desktopbuild.sh
+++ desktopbuild.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt -y install gcc libboost-all-dev build-essential cmake pkg-config libssl-dev gpp
-
-make
+echo How many cores would you like to compile with?
+read CORE
+make -j $CORE
 cd ../
 sudo chown -R $SUDO_USER:$SUDO_USER Project-Nodoze/
